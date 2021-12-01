a30c34d5fe1fc19d56bcb7cf05c38d0034d41613
kno42 <jakub.novotny@ambica.cz>
2021-12-01 11:29:08 +0100

    5

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -9,16 +9,10 @@
 
 {{ config(materialized='table') }}
 
-with source_data as (
 
-    select 1 as id
-    union all
-    select null as id
 
-)
-
-select *
-from source_data
+select id, customer
+from test
 
 /*
     Uncomment the line below to remove records with null `id` values
